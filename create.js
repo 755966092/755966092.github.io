bcead2297dd95ee5bded02a60ec2795fd57bca7c
Mr.Yuan <755966092@QQ.COM>
2022-04-06 12:04:44 +0800

    2022-04-06 12:04:43

---
+++ create.js
@@ -0,0 +1,90 @@
+
+// 下单接口
+
+// https://api-h5.ibox.art/nft-mall-web/v1/nft/order/create
+
+albumId: 100513774
+gId: 101085401
+gNum: 4170
+payChannel: 22
+price: "33.95"
+subType: null
+type: 0
+
+
+:authority: api-h5.ibox.art
+:method: POST
+:path: /nft-mall-web/v1/nft/order/create
+:scheme: https
+accept: application/json, text/plain, */*
+accept-encoding: gzip, deflate, br
+accept-language: zh-CN
+content-length: 105
+content-type: application/json
+cookie: JSESSIONID=624ce94a3659e79b46a05710e85380501e6b3102; acw_sc__v2=624ce94a3659e79b46a05710e85380501e6b3102; acw_tc=1bdd1e1c16492101368745972e5e85912e845de47f0c1336d7cf272e3b; acw_sc__v3=624cf31cacbadc67c4105cb600154ab7beb0a908; ssxmod_itna=WqAx2DRQDQDtP0LqnDjhDRxOFY0Qa=zDyY=ix0v2oGzDAxn40iDtxxTnjDhn2nQ4PqNiD6DcY452AqaXaAtUuioD=xYQDwxYoDUxGtDpxG=oil3zQDWDYPGWDDRFDRxY=DuEKDaawqDf2DAUMGqGwIM3DGupSR+037DPnYTUUR4DHBhDvYD+1iAxNw+5r4hG3KEhiZYh4l0wxbp4NvcGfIRD3KiD; ssxmod_itna2=WqAx2DRQDQDtP0LqnDjhDRxOFY0Qa=zDyY=Di5ikdDl14QIyiwxLx03KKzgngw776BI5igfGByAK9Y8nbbtcKtDxhk=0CPq54=Lz=MaqdPzwD0Cezmqe+IA2AI1+yWTk7Zibdll1gCq6wHWh0Y7=0hSGDTBWchNCYK7D08DY9WYA2q4pvHMWhien4A90ivdi8qeUwsKArv7n7F+KmxqBhDD=
+if (param["sp"] > 2147483647) {
+        UtilTool.error(
+          formatMessage({
+            id: "attentionMsg-ipt-error",
+            defaultMessage: "输入有误！",
+          }),
+          "message"
+        );
+        return;
+      }
+user-agent: Mozilla/5.0 (Linux; Android 8.0.0; SM-G955U Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36
+
+
+
+
+
+albumId: 100513726
+albumName: "iBox赛博猫狗系列"
+authorAvatar: "/file/oss/test/image/nft-goods/e4445769f5614791addecb9040092f0c.png"
+authorHeadImage: "/file/oss/test/image/nft-goods/e4445769f5614791addecb9040092f0c.png"
+authorId: 10133453
+authorName: "iBox"
+authorSocialInfo: ""
+authorWalletInfo: "0x1cfcef83638368e84c048ad34b7b2a5132781d9b"
+blindAlbumId: null
+brandDesc: "全球领先的数字藏品电商平台"
+brandId: 100000015
+brandName: "iBox"
+contractAddress: ""
+contractType: null
+gDesc: "原宇宙时代神秘组织CyberMeta的领队，代号Alpha。智商超群，体能强悍，负责小队复杂任务计划制定；执勤期间行思敏捷，冷静果断，从不拖泥带水，有着不可撼动的领导地位。"
+gDetailUrls: ""
+gFeatures: []
+gId: 100945129
+gName: "iBox−赛博边牧"
+gNum: "9951"
+gUrls: [{size: 3161447, width: 300, type: "image/jpeg",…}]
+introduction: "未来是一个万物上链的数字世界，数字藏品应用不再局限于艺术品、收藏品，将会扩展到数字身份、版权许可、品牌授权等多个领域，iBox的使命是构建一个全球领先的数字藏品电商平台。"
+isBuy: 0
+isWithdraw: 0
+lockUid: null
+newResellAddress: ""
+onSale: 6
+onSaleTime: 1646319600000
+orderId: ""
+orderStatus: ""
+origin: 0
+ownerAvatar: "/file/oss/test/image/portrait/4330c2fbb19c4d0dbb0a031416066cf1.jpg"
+ownerId: 10311927
+ownerName: "老板发财丶"
+ownerSocialInfo: ""
+ownerWalletInfo: "0xbea1fd9b26e6442d2bf6a38e7ecb264b5ba099d4"
+price: "123.32"
+priceCny: "123.32"
+priceUnit: ""
+resell: 1
+sellLimit: 57000
+sellType: 1
+synthesis: 0
+syntheticActivities: []
+thirdContractAddress: ""
+thumbPic: "/file/oss/test/image/nft-goods/d42d329590b747329f40d83affb52d1e.png"
+thumbPic2: "/file/oss/test/image/nft-goods/df663291b2f545f2b0d697a8aab0d860.jpg"
+tokenAddress: ""
+tokenId: "701160"
+vFlag: 1
