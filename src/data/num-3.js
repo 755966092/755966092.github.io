47e6da8099ddbc043bc2e6d6de6159029c903ff8
Mr.Yuan <755966092@QQ.COM>
2022-04-12 10:09:48 +0800

    2022-04-12 10:09:46

--- src/data/num-3.js
+++ src/data/num-3.js
@@ -1,14 +1,1 @@
-var lineData = {
-  "伊爱娃（IARA）": 587,
-  "佛里奥索（FURIOSA）": 657,
-  "贝贝（BEBE）": 636,
-  "普普拉（PURPURA）": 1730,
-  "拉万达（LAVANDA）": 1750,
-  "布兰卡（BRANCA）": 1743,
-  "紫月（MINGUANTE）": 1760,
-  "橙日（CRESCENTE）": 1759,
-  "蓝日（NOVA）": 1790,
-  "昂蒂娜（ONDINHA）": 1758,
-  "维洛（VELOZ）": 1765,
-  "艾斯波列塔（ESPOLETA）": 1748,
-};
+var lineData = {"伊爱娃（IARA）":597,"佛里奥索（FURIOSA）":671,"贝贝（BEBE）":645,"普普拉（PURPURA）":1767,"拉万达（LAVANDA）":1783,"布兰卡（BRANCA）":1783,"紫月（MINGUANTE）":1792,"橙日（CRESCENTE）":1797,"蓝日（NOVA）":1820,"昂蒂娜（ONDINHA）":1783,"维洛（VELOZ）":1801,"艾斯波列塔（ESPOLETA）":1785}
--- src/data/price-3.js
+++ src/data/price-3.js
@@ -1,14 +1,1 @@
-var lineData = {
-  "伊爱娃（IARA）": 16554,
-  "佛里奥索（FURIOSA）": 4437.99,
-  "贝贝（BEBE）": 4444.44,
-  "普普拉（PURPURA）": 548.9,
-  "拉万达（LAVANDA）": 555,
-  "布兰卡（BRANCA）": 550,
-  "紫月（MINGUANTE）": 555,
-  "橙日（CRESCENTE）": 555,
-  "蓝日（NOVA）": 549,
-  "昂蒂娜（ONDINHA）": 555.55,
-  "维洛（VELOZ）": 555,
-  "艾斯波列塔（ESPOLETA）": 550,
-};
+var lineData = {"伊爱娃（IARA）":16768.99,"佛里奥索（FURIOSA）":4979,"贝贝（BEBE）":5049,"普普拉（PURPURA）":856,"拉万达（LAVANDA）":849,"布兰卡（BRANCA）":875,"紫月（MINGUANTE）":858,"橙日（CRESCENTE）":846.8,"蓝日（NOVA）":840,"昂蒂娜（ONDINHA）":850,"维洛（VELOZ）":832,"艾斯波列塔（ESPOLETA）":870}
