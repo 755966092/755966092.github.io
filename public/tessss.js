216b520a589f787fffcafd64d039c447c7187160
Mr.Yuan <755966092@QQ.COM>
2022-05-02 14:48:23 +0800

    2022-05-02 14:48:22

---
+++ public/tessss.js
@@ -0,0 +1,913 @@
+var all = [
+  {
+    id: 100099581,
+    name: "《山海经》-蚩尤",
+    total: 10,
+    albumName: "山海经",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100099581",
+    authentication: "该作品拥有iBox官方认证",
+    number: 1
+  },
+  {
+    id: 100099582,
+    name: "《山海经》-呲铁",
+    total: 100,
+    albumName: "山海经",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100099582",
+    authentication: "该作品拥有iBox官方认证",
+    number: 2
+  },
+  {
+    id: 100099599,
+    name: "《山海经》-狡",
+    total: 204,
+    albumName: "山海经",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100099599",
+    authentication: "该作品拥有iBox官方认证",
+    number: 3
+  },
+  {
+    id: 100000395,
+    name: "《山海经》-狰",
+    total: 500,
+    albumName: "山海经",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100000395",
+    authentication: "该作品拥有iBox官方认证",
+    number: 4
+  },
+  {
+    id: 100000378,
+    name: "《山海经》-混沌",
+    total: 6888,
+    albumName: "山海经",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100000378",
+    authentication: "该作品拥有iBox官方认证",
+    number: 5
+  },
+  {
+    id: 100513734,
+    name: "《山海经》-何罗鱼",
+    total: 500,
+    albumName: "山海经",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513734",
+    authentication: "该作品拥有iBox官方认证",
+    number: 6
+  },
+  {
+    id: 100513733,
+    name: "《山海经》-雷神",
+    total: 100,
+    albumName: "山海经",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513733",
+    authentication: "该作品拥有iBox官方认证",
+    number: 7
+  },
+  {
+    id: 100513748,
+    name: "《山海经》-钦原",
+    total: 314,
+    albumName: "山海经",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513748",
+    authentication: "该作品拥有iBox官方认证",
+    number: 8
+  },
+  {
+    id: 100513745,
+    name: "火与工匠之神-赫菲斯托斯",
+    total: 503,
+    albumName: "iBox纪念系列",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513745",
+    authentication: "该作品拥有iBox官方认证",
+    number: 9
+  },
+  {
+    id: 100513777,
+    name: "神王-宙斯",
+    total: 1100,
+    albumName: "希腊神话系列",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513777",
+    authentication: "该作品拥有iBox官方认证",
+    number: 10
+  },
+  {
+    id: 100513778,
+    name: "冥王-哈迪斯",
+    total: 1640,
+    albumName: "希腊神话系列",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513778",
+    authentication: "该作品拥有iBox官方认证",
+    number: 11
+  },
+  {
+    id: 100513779,
+    name: "智慧女神-雅典娜",
+    total: 1640,
+    albumName: "希腊神话系列",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513779",
+    authentication: "该作品拥有iBox官方认证",
+    number: 12
+  },
+  {
+    id: 100513780,
+    name: "狩猎女神-阿尔忒弥斯",
+    total: 800,
+    albumName: "希腊神话系列",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513780",
+    authentication: "该作品拥有iBox官方认证",
+    number: 13
+  },
+  {
+    id: 100513781,
+    name: "太阳神-阿波罗",
+    total: 300,
+    albumName: "希腊神话系列",
+    author: "李延",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513781",
+    authentication: "该作品拥有iBox官方认证",
+    number: 14
+  },
+  {
+    id: 100100115,
+    name: "金钱豹",
+    total: 500,
+    albumName: "金钱豹",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100100115",
+    authentication: "该作品拥有iBox官方认证",
+    number: 15
+  },
+  {
+    id: 100513396,
+    name: "一見有喜 平安長樂",
+    total: 2000,
+    albumName: "iBox纪念系列",
+    author: "何宽",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513396",
+    authentication: "该作品拥有iBox官方认证",
+    number: 16
+  },
+  {
+    id: 100513397,
+    name: "一見有喜 平安長樂",
+    total: 2000,
+    albumName: "iBox纪念系列",
+    author: "何宽",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513397",
+    authentication: "该作品拥有iBox官方认证",
+    number: 17
+  },
+  {
+    id: 100513731,
+    name: "未来太空之十二大事件",
+    total: 3000,
+    albumName: "“十二天宫”系列",
+    author: "王泽工作室",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513731",
+    authentication: "该作品拥有十二天宫官方认证",
+    number: 18
+  },
+  {
+    id: 100513736,
+    name: "月球探测器",
+    total: 1800,
+    albumName: "月球探测器",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513736",
+    authentication: "该作品拥有iBox官方认证",
+    number: 19
+  },
+  {
+    id: 100513737,
+    name: "暴富有道",
+    total: 333,
+    albumName: "茅山财富新福系列",
+    author: "中轴线数字天工",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513737",
+    authentication: "该作品拥有中轴线数字天工官方认证",
+    number: 20
+  },
+  {
+    id: 100513741,
+    name: "功夫小子",
+    total: 558,
+    albumName: "iBox纪念系列",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513741",
+    authentication: "该作品拥有iBox官方认证",
+    number: 21
+  },
+  {
+    id: 100513744,
+    name: "悟道小子",
+    total: 474,
+    albumName: "iBox纪念系列",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513744",
+    authentication: "该作品拥有iBox官方认证",
+    number: 22
+  },
+  {
+    id: 100513727,
+    name: "iBox-虎头小子",
+    total: 500,
+    albumName: "iBox纪念系列",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513727",
+    authentication: "该作品拥有iBox官方认证",
+    number: 23
+  },
+  {
+    id: 100000169,
+    name: "闪电（Flash）",
+    total: 15000,
+    albumName: "闪电（Flash）",
+    author: "GfanX Inc.",
+    link: "https://www.ibox.art/zh-cn/item/?id=100000169",
+    authentication: "该作品拥有GfanX Inc.官方认证",
+    number: 24
+  },
+  {
+    id: 100000128,
+    name: "《找自己》快乐版",
+    total: 10000,
+    albumName: "iBox先锋音乐",
+    author: "iFans NFT",
+    link: "https://www.ibox.art/zh-cn/item/?id=100000128",
+    authentication: "该作品拥有iFans NFT官方认证",
+    number: 25
+  },
+  {
+    id: 100000118,
+    name: "iBox 002号空投版",
+    total: 20000,
+    albumName: "iBox 002号空投版",
+    author: "iFans NFT",
+    link: "https://www.ibox.art/zh-cn/item/?id=100000118",
+    authentication: "该作品拥有iFans NFT官方认证",
+    number: 26
+  },
+  {
+    id: 100513752,
+    name: "万国争霸-资源徽章",
+    total: 600,
+    albumName: "万国争霸-资源徽章",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513752",
+    authentication: "该作品拥有iBox官方认证",
+    number: 27
+  },
+  {
+    id: 100513756,
+    name: "Love-001",
+    total: 200,
+    albumName: "Love-001",
+    author: "Otatob",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513756",
+    authentication: "该作品拥有iBox官方认证",
+    number: 28
+  },
+  {
+    id: 100513758,
+    name: "潇湘咖啡",
+    total: 700,
+    albumName: "潇湘咖啡",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513758",
+    authentication: "该作品拥有iBox官方认证",
+    number: 29
+  },
+  {
+    id: 100513790,
+    name: "霸王别姬",
+    total: 300,
+    albumName: "梅兰芳-霸王别姬",
+    author: "华韵传媒股份有限公司",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513790",
+    authentication: "该作品拥有中轴线数字天工官方认证",
+    number: 30
+  },
+  {
+    id: 100513798,
+    name: "秋色斑斓",
+    total: 500,
+    albumName: "红红火火",
+    author: "刘向东",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513798",
+    authentication: "该作品拥有深圳市小马腾云互动科技有限公司官方认证",
+    number: 31
+  },
+  {
+    id: 100513799,
+    name: "金瓜满载",
+    total: 600,
+    albumName: "红红火火",
+    author: "刘向东",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513799",
+    authentication: "该作品拥有深圳市小马腾云互动科技有限公司官方认证",
+    number: 32
+  },
+  {
+    id: 100513795,
+    name: "森林聚会",
+    total: 300,
+    albumName: "森林聚会",
+    author: "喵小蛋",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513795",
+    authentication: "该作品拥有元宙光年官方认证",
+    number: 33
+  },
+  {
+    id: 100513796,
+    name: "异世界·海之城",
+    total: 450,
+    albumName: "异世界·海之城",
+    author: "爱豆",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513796",
+    authentication: "该作品拥有元宙光年官方认证",
+    number: 34
+  },
+  {
+    id: 100000120,
+    name: "GGAC 探索地球起源传说",
+    total: 30,
+    albumName: "GGAC 探索地球起源传说",
+    author: "GGAC",
+    link: "https://www.ibox.art/zh-cn/item/?id=100000120",
+    authentication: "该作品拥有GGAC.COM CG艺术创作分享平台官方认证",
+    number: 35
+  },
+  {
+    id: 100513726,
+    name: "iBox−赛博边牧",
+    total: 57000,
+    albumName: "iBox赛博猫狗系列",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513726",
+    authentication: "该作品拥有iBox官方认证",
+    number: 36
+  },
+  {
+    id: 100513782,
+    name: "iBox-CyberDragon",
+    total: 33844,
+    albumName: "iBox赛博生肖系列",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513782",
+    authentication: "该作品拥有iBox官方认证",
+    number: 37
+  },
+
+  {
+    id: 100099867,
+    name: "泰迪熊-空投版",
+    total: 5000,
+    albumName: "泰迪熊-空投版",
+    author: "至深无下贸易（上海）有限公司",
+    link: "https://www.ibox.art/zh-cn/item/?id=100099867",
+    authentication: "该作品拥有iBox官方认证",
+    number: 38
+  },
+  {
+    id: 100000396,
+    name: "iBox宣传小标兵",
+    total: 2500,
+    albumName: "iBox纪念系列",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100000396",
+    authentication: "该作品拥有iBox官方认证",
+    number: 39
+  },
+  {
+    id: 100000335,
+    name: "iBox-潮牛逐浪",
+    total: 30000,
+    albumName: "iBox礼遇系列 ",
+    author: "mococo",
+    link: "https://www.ibox.art/zh-cn/item/?id=100000335",
+    authentication: "该作品拥有iBox官方认证",
+    number: 40
+  },
+  {
+    id: 100000254,
+    name: "iBox-玄牛遂意",
+    total: 40000,
+    albumName: "iBox礼遇系列",
+    author: "mococo",
+    link: "https://www.ibox.art/zh-cn/item/?id=100000254",
+    authentication: "该作品拥有iBox官方认证",
+    number: 41
+  },
+  {
+    id: 100000253,
+    name: "iBox-灵牛来喜",
+    total: 20000,
+    albumName: "iBox礼遇系列",
+    author: "mococo",
+    link: "https://www.ibox.art/zh-cn/item/?id=100000253",
+    authentication: "该作品拥有iBox官方认证",
+    number: 42
+  },
+  {
+    id: 100000190,
+    name: "iBox-神牛降瑞",
+    total: 10000,
+    albumName: "iBox礼遇系列",
+    author: "mococo",
+    link: "https://www.ibox.art/zh-cn/item/?id=100000190",
+    authentication: "该作品拥有iBox官方认证",
+    number: 43
+  },
+  {
+    id: 100000139,
+    name: "iBox-金牛献福",
+    total: 10000,
+    albumName: "iBox礼遇系列",
+    author: "mococo",
+    link: "https://www.ibox.art/zh-cn/item/?id=100000139",
+    authentication: "该作品拥有iBox官方认证",
+    number: 44
+  },
+  {
+    id: 100000388,
+    name: "iBox-战牛无双",
+    total: 4500,
+    albumName: "iBox礼遇系列",
+    author: "mococo",
+    link: "https://www.ibox.art/zh-cn/item/?id=100000388",
+    authentication: "该作品拥有iBox官方认证",
+    number: 45
+  },
+  {
+    id: 100000394,
+    name: "iBox-翔牛摘星",
+    total: 10000,
+    albumName: "iBox礼遇系列",
+    author: "mococo",
+    link: "https://www.ibox.art/zh-cn/item/?id=100000394",
+    authentication: "该作品拥有iBox官方认证",
+    number: 46
+  },
+  {
+    id: 100000384,
+    name: "iBox-电牛放歌",
+    total: 3000,
+    albumName: "iBox礼遇系列",
+    author: "mococo",
+    link: "https://www.ibox.art/zh-cn/item/?id=100000384",
+    authentication: "该作品拥有iBox官方认证",
+    number: 47
+  },
+  {
+    id: 100099573,
+    name: "集牛换虎-虎虎生威",
+    total: 549,
+    albumName: "iBox礼遇系列",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100099573",
+    authentication: "该作品拥有iBox官方认证",
+    number: 48
+  },
+  {
+    id: 100513724,
+    name: "iBox-CyberTiger",
+    total: 53000,
+    albumName: "iBox赛博生肖系列",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513724",
+    authentication: "该作品拥有iBox官方认证",
+    number: 49
+  },
+  {
+    id: 100513750,
+    name: "iBox−赛博蓝猫",
+    total: 14991,
+    albumName: "iBox赛博猫狗系列",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513750",
+    authentication: "该作品拥有iBox官方认证",
+    number: 52
+  },
+  {
+    id: 100513774,
+    name: "iBox−赛博布偶",
+    total: 50000,
+    albumName: "iBox赛博猫狗系列",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513774",
+    authentication: "该作品拥有iBox官方认证",
+    number: 51
+  },
+  {
+    id: 100000393,
+    name: "I‘m Sorry",
+    total: 100000,
+    albumName: "iBox纪念系列",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100000393",
+    authentication: "该作品拥有iBox官方认证",
+    number: 50
+  },
+
+  // -------2022-04-15 17:31:20------
+  {
+    id: 100513807,
+    name: "大鱼",
+    total: 3333,
+    albumName: "大鱼",
+    author: "西图",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513807",
+    authentication: "该作品拥有Magicbox官方认证",
+    number: 53
+  },
+  {
+    id: 100513800,
+    name: "爱丽丝梦游仙境",
+    total: 3333,
+    albumName: "爱丽丝梦游仙境",
+    author: "西图",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513800",
+    authentication: "该作品拥有Magicbox官方认证",
+    number: 54
+  },
+  {
+    id: 100513802,
+    name: "五虎上将-赵云",
+    total: 200,
+    albumName: "五虎上将",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513802",
+    authentication: "该作品拥有iBox官方认证",
+    number: 55
+  },
+  {
+    id: 100513803,
+    name: "思情郎",
+    total: 400,
+    albumName: "思情郎",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513803",
+    authentication: "该作品拥有恩爱福提官方认证",
+    number: 56
+  },
+  {
+    id: 100513804,
+    name: "iBox-赛博加菲",
+    total: 32000,
+    albumName: "iBox赛博猫狗系列",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513804",
+    authentication: "该作品拥有iBox官方认证",
+    number: 57
+  },
+  {
+    id: 100513805,
+    name: "iBox-赛博哈士奇",
+    total: 12000,
+    albumName: "iBox赛博猫狗系列",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513805",
+    authentication: "该作品拥有iBox官方认证",
+    number: 58
+  },
+  {
+    id: 100513806,
+    name: "iBox-赛博司芬克斯",
+    total: 12000,
+    albumName: "iBox赛博猫狗系列",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513806",
+    authentication: "该作品拥有iBox官方认证",
+    number: 59
+  },
+  {
+    id: 100513819,
+    name: "五虎上将-关羽",
+    total: 500,
+    albumName: "五虎上将",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513819",
+    authentication: "该作品拥有iBox官方认证",
+    number: 61
+  },
+  {
+    id: 100513822,
+    name: "五虎上将-马超",
+    total: 500,
+    albumName: "五虎上将",
+    author: "iBox",
+    link: "https://www.ibox.art/zh-cn/item/?id=100513822",
+    authentication: "该作品拥有iBox官方认证",
+    number: 62
+  },
+  {
+    id: 100513845,
+    name: "威尼斯双年艺术展纪念海报",
+
+    authentication: "杨嘎",
+
+    authentication: "YG加密艺术工作室",
+
+    gDetailUrls: "",
+    gFeatures: [],
+
+    gName: "威尼斯双年艺术展纪念海报",
+    gNum: "1573",
+    gUrls: [
+      {
+        size: 0,
+        videoUrl: "/file/oss/test/video/nft-mall/b1d2e4fcd6fd4faba76f36a895f98aeb.mp4",
+        width: 1048,
+
+        type: "video/mp4",
+        mediaPlayMode: 1,
+        url: "/file/oss/test/image/nft-goods/f056120307f3432da4668303e8c75883.png",
+        height: 1444
+      }
+    ],
+
+    isBuy: 0,
+    isWithdraw: 0,
+
+    origin: 0,
+
+    author: "杨嘎",
+
+    price: "",
+    priceCny: "99",
+
+    total: 2000,
+
+    vFlag: 1
+  },
+
+  {
+    id: 100513847,
+    name: "COSMOS-太空龙",
+
+    authentication: "Pancya",
+
+    authentication: "NFTCOSMOS",
+
+    gDetailUrls: "",
+    gFeatures: [],
+
+    gName: "COSMOS-太空龙",
+    gNum: "1777",
+    gUrls: [
+      {
+        size: 887051,
+        width: 300,
+        type: "image/png",
+        url: "/file/oss/test/image/nft-goods/8c06b07c02384a8facc9b306c2af5471.png",
+        height: 400
+      }
+    ],
+
+    isBuy: 1,
+    isWithdraw: 0,
+
+    origin: 0,
+
+    author: "四月限定·大白",
+
+    price: "5450",
+    priceCny: "5450",
+
+    total: 3000,
+
+    vFlag: 1
+  },
+
+  {
+    id: 100513848,
+    name: "长安往事",
+
+    authentication: "张肉丸",
+
+    authentication: "MagicBox",
+
+    gDetailUrls: "",
+    gFeatures: [],
+
+    gName: "鱼化寨由来",
+    gNum: "1293",
+    gUrls: [
+      {
+        size: 7327487,
+        width: 300,
+        type: "image/jpeg",
+        url: "/file/oss/test/image/nft-goods/d510918d94f145a0b491827f426d35a7.jpg",
+        height: 400
+      }
+    ],
+
+    isBuy: 1,
+    isWithdraw: 0,
+
+    origin: 0,
+
+    author: "llll",
+
+    price: "7259",
+    priceCny: "7259",
+
+    total: 2000,
+
+    vFlag: 1
+  },
+
+  {
+    id: 100513850,
+    name: "迷幻梦境",
+
+    authentication: "西图",
+
+    authentication: "MagicBox",
+
+    gDetailUrls: "",
+    gFeatures: [],
+
+    gName: "迷幻梦境",
+    gNum: "42",
+    gUrls: [
+      {
+        size: 1355733,
+        width: 300,
+        type: "image/jpeg",
+        url: "/file/oss/test/image/nft-goods/6071d3b45dc540abb88e2389cbfbda5e.jpg",
+        height: 400
+      }
+    ],
+
+    isBuy: 1,
+    isWithdraw: 0,
+
+    origin: 0,
+
+    author: "白日梦想家🥳",
+
+    price: "42000",
+    priceCny: "42000",
+
+    total: 300,
+
+    vFlag: 1
+  },
+
+  {
+    id: 100513851,
+    name: "呼唤",
+
+    authentication: "iBox",
+
+    authentication: "iBox",
+
+    gDetailUrls: "",
+    gFeatures: [],
+
+    gName: "呼唤",
+    gNum: "223",
+    gUrls: [
+      {
+        size: 8423310,
+        videoUrl: "/file/oss/test/video/nft-mall/202a49ad31e341dab560d527431a9038.mp4",
+        width: 1080,
+
+        type: "video/mp4",
+        mediaPlayMode: 1,
+        url: "/file/oss/test/image/nft-goods/0ed1ccd1e57b403396cff56866bc70fc.png",
+        height: 1080
+      }
+    ],
+
+    isBuy: 1,
+    isWithdraw: 0,
+
+    origin: 0,
+
+    author: "春天花又来",
+
+    price: "31588",
+    priceCny: "31588",
+
+    total: 600,
+
+    vFlag: 1
+  },
+
+  {
+    id: 100513852,
+    name: "蝴蝶精灵",
+
+    authentication: "西图",
+
+    authentication: "MagicBox",
+
+    gDetailUrls: "",
+    gFeatures: [],
+
+    gName: "蝴蝶精灵",
+    gNum: "947",
+    gUrls: [
+      {
+        size: 757851,
+        width: 300,
+        type: "image/jpeg",
+        url: "/file/oss/test/image/nft-goods/39bd051051d74cda918c198288ef04e0.jpg",
+        height: 400
+      }
+    ],
+
+    isBuy: 1,
+    isWithdraw: 0,
+
+    origin: 0,
+
+    author: "一路有阳光",
+
+    price: "18488.88",
+    priceCny: "18488.88",
+
+    total: 3000,
+
+    vFlag: 1
+  },
+
+  {
+    id: 100513853,
+    name: "情愫",
+
+    authentication: "iBox",
+
+    authentication: "iBox",
+
+    gDetailUrls: "",
+    gFeatures: [],
+
+    gName: "情愫",
+    gNum: "104",
+    gUrls: [
+      {
+        size: 10147181,
+        videoUrl: "/file/oss/test/video/nft-mall/9e56e8c0c944468185d2cffeb83f5212.mp4",
+        width: 1080,
+
+        type: "video/mp4",
+        mediaPlayMode: 1,
+        url: "/file/oss/test/image/nft-goods/7d56003e1f37492aaa5eda055c209882.png",
+        height: 1080
+      }
+    ],
+
+    isBuy: 1,
+    isWithdraw: 0,
+
+    origin: 0,
+
+    author: "先锋五百加",
+
+    price: "30000",
+    priceCny: "30000",
+
+    total: 600,
+
+    vFlag: 1
+  }
+];
+
+var a = [];
+var b = [];
+
+for (let i = 0; i < all.length; i++) {
+  const element = all[i];
+  if (element.total > 10000) {
+    a.push(element.id);
+  } else {
+    b.push(element.id);
+  }
+}
+console.log(a);
+console.log(b);
+
+var a10000 = [
+  100000169, 100000118,
+  100513726, 100513782,
+  100000335, 100000254,
+  100000253, 100513724,
+  100513750, 100513774,
+  100000393, 100513804,
+  100513805, 100513806
+]
