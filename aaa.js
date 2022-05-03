47af93d6ef3e0d1d2756f0cbaebc68cd3f2712ee
Mr.Yuan <755966092@QQ.COM>
2022-05-03 13:56:32 +0800

    2022-05-03 13:56:29

--- aaa.js
+++
@@ -1,6 +0,0 @@
-
-const uuid = require('node-uuid')
-uuid.v4().replace(/\-/g,'')
-var v1 = uuid.v1()
-console.log('uuid.v1(): ', uuid.v1().replace(/\-/g,''));
-console.log('uuid.v4(): ', uuid.v4().replace(/\-/g,''));
