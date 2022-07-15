3aa090faf03e4e5d02342551962d1523b02209fc
Mr.Yuan <755966092@QQ.COM>
2022-07-15 15:56:22 +0800

    a

--- ddts钉钉推送.js
+++ ddts钉钉推送.js
@@ -126,41 +126,56 @@ const addressList = [
   },
   {
     address: "0x534e76131946aebfb4ee296365f8c81182acab95",
-    name: "NEI-罗纳威犬-1"
+    name: "NEI-罗纳威犬-1个"
   },
   {
     address: "0x3700ba3feae4cd9f5042173eeca8a6794442a1e9",
-    name: "NEI-罗纳威犬-5-柒岁"
+    name: "NEI-罗纳威犬-5个-柒岁"
   },
   {
-    name: "罗纳威犬-两个-2",
+    name: "罗纳威犬-两个-2个",
     address: "0x472080f9b53526fececbe9cd96920e9a34c106f9"
   },
   {
-    name: "罗纳威犬-长安-1",
+    name: "罗纳威犬-长安-1个",
     address: "0xf6336d69e5822214d497d4ac2840e5aa2de37027"
   },
 
   {
-    name: "罗纳威犬-两个罗纳威犬-2",
+    name: "罗纳威犬-两个罗纳威犬-2个",
     address: "0x7cec63ed64fdfdaa2a7c9bbf5c461fe5eacac674"
   },
   {
-    name: "罗纳威犬-业精于勤",
+    name: "罗纳威犬-业精于勤-1个",
     address: "0x3ff690b2b5320e5ddacad8b9addb61c97167638b"
   },
   {
-    name: "罗纳威犬-bug我关羽留v",
+    name: "罗纳威犬-bug我关羽留v-1个",
     address: "0xc087424b5495b4812df10b03b8ea84387206fa0f"
   },
   {
-    name: "罗纳威犬-干",
+    name: "罗纳威犬-干-1个",
     address: "0x334ec89d987bc76a5b443950d7f55a89f17850e7"
   },
   {
-    name: "罗纳威犬-跑跑-2",
+    name: "罗纳威犬-跑跑很准-2个",
     address: "0xc32c4ef06d3e70ea32a006d752ecec38f9087f7c"
-  }
+  },
+  {
+    userid: 12516366,
+    name: "多维合成-小目标-1个",
+    address: "0xea50bd7a3ebe14d208bd50cdad027780c3871271",
+  },
+  {
+    userid: 10441361,
+    name: "多维合成-割割不要再割了-1个",
+    address: "0x31b52751b3ab0d270fad9f0974aa9930287ef502",
+  },
+  {
+    userid: 10966600,
+    name: "多维合成-相信资本的力量-2个",
+    address: "0xc695473e4a10fc9fca0c92804e5a7e9ba4c9cbb8",
+  },
 ];
 var addressData = {};
 let getFlag = true;
@@ -293,10 +308,13 @@ function sendNews(news, params) {
     "0x26028941e28ad3eae79543c6768595d4d4daaab4",
     "0xfe4f8cb9b5861efc496ee07ad0cc74e3363b363e",
     "0x06fefec3d5d978aea8ca1367421c9d78b0adef83",
-    "0x05c98f6be94182bfa0ff1dd18f7c3b93cff18a37"
+    "0x05c98f6be94182bfa0ff1dd18f7c3b93cff18a37",
+    "0xc32c4ef06d3e70ea32a006d752ecec38f9087f7c",
+    "0x3700ba3feae4cd9f5042173eeca8a6794442a1e9"
   ];
   if (noall.indexOf(params.address) == -1) {
     sendNews2(news);
+    sendNews3(news);
   }
   request(
     {
@@ -331,6 +349,27 @@ function sendNews2(news) {
         markdown: news
       })
       // body: JSON.stringify({ msgtype: "text", text: { content: news } })
+    },
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+      }
+    }
+  );
+}
+
+// 新老监控群
+function sendNews3(news) {
+  request(
+    {
+      url: "https://oapi.dingtalk.com/robot/send?access_token=efaeda965fb6a4f4742cd5c29e4f8a72c7bd7df5502ef77aa4ba2e6340b50391", //请求路径
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        msgtype: "markdown",
+        markdown: news
+      })
     },
     function (error, response, body) {
       if (!error && response.statusCode == 200) {
