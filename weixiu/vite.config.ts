c3ac77cea66ba19bc66724be11222bca75861397
Mr.Yuan <755966092@QQ.COM>
2025-08-19 11:19:52 +0800

    build: update vite config and gitignore

--- weixiu/vite.config.ts
+++ weixiu/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/*
+ * @Author: yuanzhuangzhuang@jujin8.com
+ * @Date: 2025-07-25 09:30:46
+ * @LastEditors: yuanzhuangzhuang@jujin8.com
+ * @LastEditTime: 2025-08-19 11:19:12
+ * @FilePath: /master/weixiu/vite.config.ts
+ * @Description: 文件注释
+ */
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "./",
+  plugins: [react()]
+});
